04f2d16411ca058deacd7e63ec9d218c60d68b54
Bastien Abadie <bastien.abadie@gmail.com>
2013-02-07 23:45:47 +0100

    Use bootstrap :D

--- coach/urls.py
+++ coach/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from coach.settings import MEDIA_ROOT
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,4 +8,7 @@ urlpatterns = patterns('',
   url(r'^user/', include('users.urls')),
 
   url(r'^admin/', include(admin.site.urls)),
+
+  # Medias for dev
+  (r'^medias/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
 )
