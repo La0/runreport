43eb70fe55e7f2b2abd2b7ffd17644258637e083
Bastien Abadie <bastien.abadie@gmail.com>
2013-02-11 23:59:38 +0100

    Add command to publish available reports

---
+++ run/management/commands/publish_reports.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from datetime import date
+from run.models import RunReport
+
+class Command(BaseCommand):
+
+  def handle(self, *args, **options):
+    '''
+    Publish all reports for this week
+    '''
+    year, week = self.get_current_week()
+    reports = RunReport.objects.filter(year=year, week=week, published=False).order_by('user__username')
+    for r in reports:
+
+      # Skip empty report
+      sessions = r.sessions.exclude(comment=None)
+      if sessions.count() == 0:
+        print 'No active sessions for report %s' % r
+        continue
+
+      # Publish
+      r.publish()
+      print 'Published %s' % r
+
+  def get_current_week(self):
+    today = date.today()
+    return today.year, int(today.strftime('%W'))
