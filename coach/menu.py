69723b922d5a43a7b76fb3abf92a06cdac7e6efd
Bastien Abadie <bastien.abadie@gmail.com>
2013-02-24 18:59:41 +0100

    Display clubs links when connected, using dropdowns.

--- coach/menu.py
+++ coach/menu.py
@@ -10,12 +10,31 @@ def add_pages(request):
     active = lazy and request.path.startswith(url) or (request.path == url)
     return {'url' : url, 'caption' : caption, 'active' : active}
 
+  def _ext(url, caption):
+    return {'url' : url, 'caption' : caption, 'active' : False, 'external' : True}
+
   menu = []
   if request.user.is_authenticated():
     menu.append(_p('report-current', 'Cette semaine'))
     menu.append(_p('report-current-month', 'Calendrier', True))
-    menu.append(_p('user-profile', 'Mon profil'))
-    menu.append(_p('logout', u'Se déconnecter'))
+
+    # Club menu
+    submenu = {
+      'caption' : 'Le club',
+      'menu' : []
+    }
+    submenu['menu'].append(_ext('http://csternes.athle.org', 'Site officiel'))
+    submenu['menu'].append(_ext('http://facebook.com/groups/USA17', 'Groupe Facebook'))
+    menu.append(submenu)
+
+    # User menu
+    submenu = {
+      'caption' : request.user.first_name,
+      'menu' : []
+    }
+    submenu['menu'].append(_p('user-profile', 'Mon profil'))
+    submenu['menu'].append(_p('logout', u'Se déconnecter'))
+    menu.append(submenu)
   else:
     menu.append(_p('user-create', u'Créer un compte'))
     menu.append(_p('login', 'Se connecter'))
