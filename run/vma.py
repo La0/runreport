de445f52a03eeb7ef53fdba7a0c827c71fd086b9
Bastien Abadie <bastien.abadie@gmail.com>
2013-02-27 23:49:55 +0100

    Add time displays for a given distance / pace. Simplify vma api.

--- run/vma.py
+++ run/vma.py
@@ -15,7 +15,11 @@ class VmaCalc:
     Pace('EMA 3', 90),
     Pace('VMA L', 95),
     Pace('VMA C', 100),
+    Pace('A fond', 105),
+    Pace('Max', 110),
   ]
+
+  _distances = [100, 200, 300, 400, 500, 600, 800, 1000, 1500, 2000, 3000, 4000, 5000, 10000]
 
   def __init__(self, vma):
     self._vma = vma
@@ -23,17 +27,18 @@ class VmaCalc:
   def get_paces(self):
     return self._paces
 
-  def get_speeds(self, distances=(1000, 100)):
-    out = {}
-    for pace in self._paces:
-      speed = self._vma * pace.percent / 100.0 
-      pace_data = {
-        'speed' : speed,
-      }
-      for dist in distances:
-        t = time.localtime((dist * 3600) / (speed * 1000))
-        pace_data[dist] = datetime.time(t.tm_hour, t.tm_min, t.tm_sec)
-      out[pace] = pace_data
+  def get_distances(self):
+    return self._distances
 
-    return out
+  def get_speed(self, pace):
+    '''
+    Get the speed in km/h at a pace
+    '''
+    return self._vma * pace.percent / 100.0
 
+  def get_time(self, pace, distance):
+    '''
+    Get the time object needed to run a distance at a pace
+    '''
+    t = time.localtime((distance * 3600) / (self.get_speed(pace) * 1000))
+    return datetime.time(t.tm_hour, t.tm_min, t.tm_sec)
