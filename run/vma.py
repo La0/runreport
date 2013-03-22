b345025d23e5e061ec6b3029ed9e5db5f7eb2d9b
Bastien Abadie <babadie@ivsweb.com>
2013-03-22 18:20:14 +0100

    Apply Sylvain comments about vma names.

--- run/vma.py
+++ run/vma.py
@@ -7,16 +7,15 @@ Pace = namedtuple('Pace', ['name', 'percent'])
 class VmaCalc:
   _vma = 0.0
   _paces = [
-    Pace('Footing Lent', 50),
-    Pace('Footing 1', 60),
-    Pace('Footing 2', 70),
+    Pace('L', 50),
+    Pace('1', 60),
+    Pace('2', 70),
     Pace('EMA 1', 80),
     Pace('EMA 2', 85),
     Pace('EMA 3', 90),
     Pace('VMA L', 95),
     Pace('VMA C', 100),
-    Pace('A fond', 105),
-    Pace('Max', 110),
+    Pace('VMA C', 105),
   ]
 
   _distances = [100, 200, 300, 400, 500, 600, 800, 1000, 1500, 2000, 3000, 4000, 5000, 10000]
