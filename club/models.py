8d1c31de6093e6a126ca3fc505cea9618687f474
Bastien Abadie <bastien.abadie@gmail.com>
2013-03-31 22:16:47 +0200

    Add Club & ClubMembership Models

--- club/models.py
+++ club/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Club(models.Model):
+  name = models.CharField(max_length=250)
+  slug = models.SlugField(unique=True)
+  members = models.ManyToManyField(User, through='ClubMembership')
+
+class ClubMembership(models.Model):
+  CLUB_ROLES = (
+    ('athlete', 'Athlete'),
+    ('trainer', 'Trainer'),
+  )
+  user = models.ForeignKey(User)
+  club = models.ForeignKey(Club)
+  role = models.CharField(max_length=10, choices=CLUB_ROLES)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    unique_together = (('user', 'club'),)
