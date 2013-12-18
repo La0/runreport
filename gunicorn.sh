5a82740fa4fb464e0de9aa757b78187744c3070f
Bastien Abadie <bastien.abadie@gmail.com>
2013-12-18 09:11:30 +0100

    Support gunicorn for prod.

---
+++ gunicorn.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+ 
+NAME="coach" # Name of the application
+DJANGODIR=/var/prod/runreport
+SOCKFILE=/var/prod/runreport/gunicorn.socket
+USER=lao
+GROUP=lao
+NUM_WORKERS=3 # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=coach.settings
+DJANGO_WSGI_MODULE=coach.wsgi # WSGI module name
+ 
+echo "Starting $NAME as `whoami`"
+ 
+# Activate the virtual environment
+cd $DJANGODIR
+source env/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+ 
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+ 
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec env/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+--name $NAME \
+--workers $NUM_WORKERS \
+--user=$USER --group=$GROUP \
+--log-level=debug \
+--bind=unix:$SOCKFILE \
+--daemon # TRASHME for supervisor
