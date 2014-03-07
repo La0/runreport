08fcbcc53e21202db3e9a46f4d13d01b98ad9ab4
Bastien Abadie <bastien.abadie@gmail.com>
2014-03-07 16:22:21 +0100

    Add prod script to dump database.

---
+++ bin/dump_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+BASE=`readlink -f $( dirname "${BASH_SOURCE[0]}" )/..`
+cd $BASE
+source env/bin/activate
+
+DUMP_PATH="$BASE/db/`date +%Y%m%d`.json"
+APPS="run users club page plan
+
+./manage.py dumpdata --indent=4 -e sessions $APPS > $DUMP_PATH
