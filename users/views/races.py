43a77284a7ce278c9c936a53197bcf9278a47e2d
Bastien Abadie <bastien.abadie@gmail.com>
2014-05-17 14:21:59 +0200

    Use sessions for races list.

--- users/views/races.py
+++ users/views/races.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.db.models import Sum
 from sport.models import SportDay, RaceCategory
 from datetime import date
 
@@ -20,10 +21,13 @@ class RacesView(TemplateView):
     cat_ids= [r['race_category'] for r in past_races.values('race_category').distinct() if r['race_category']]
     categories = RaceCategory.objects.filter(pk__in=cat_ids).order_by('name')
 
+    # Sum sessions time & distance per race
+    past_races = past_races.annotate(time_total=Sum('sessions__time'), distance_total=Sum('sessions__distance'))
+
     # Categorize races
     races = {}
     for c in categories:
-      races[c.id] = past_races.filter(race_category=c).order_by('time')
+      races[c.id] = past_races.filter(race_category=c).order_by('time_total')
 
     return {
       'future_races' : future_races,
