be59b86de7f16659ceb207a4041ee2154598bb10
Bastien Abadie <bastien.abadie@gmail.com>
2014-05-23 20:51:51 +0200

    Reload day form when a session is added.

--- sport/views/session.py
+++ sport/views/session.py
@@ -2,8 +2,9 @@ from django.views.generic.edit import ModelFormMixin, ProcessFormView
 from django.views.generic import DateDetailView
 from sport.forms import SportSessionForm
 from sport.models import SportSession
-from coach.mixins import JsonResponseMixin
+from coach.mixins import JsonResponseMixin, JSON_OPTION_NO_HTML, JSON_OPTION_CLOSE
 from mixins import CalendarDay
+from django.core.urlresolvers import reverse
 
 class SportSessionAdd(CalendarDay, JsonResponseMixin, ModelFormMixin, ProcessFormView, DateDetailView):
   model = SportSession
@@ -13,8 +14,8 @@ class SportSessionAdd(CalendarDay, JsonResponseMixin, ModelFormMixin, ProcessFor
   def get_form_kwargs(self, *args, **kwargs):
     self.get_object() # Load day
 
-    # TODO in mixin
-    self.session = SportSession(sport=self.request.user.default_sport)
+    # Init a session
+    self.session = SportSession(sport=self.request.user.default_sport, day=self.object)
 
     return {
       'instance' : self.session,
@@ -28,4 +29,19 @@ class SportSessionAdd(CalendarDay, JsonResponseMixin, ModelFormMixin, ProcessFor
     context['session'] = self.session
     return context
 
+  def form_valid(self, form):
 
+    # Save session, but create day before
+    session = form.save(commit=False)
+    if not self.object.pk:
+      self.object.save()
+    session.day = self.object
+    session.save()
+
+    # Configure output
+    self.json_options = [JSON_OPTION_NO_HTML, JSON_OPTION_CLOSE]
+    self.json_boxes = {
+      'day-%s' % self.day : reverse('report-day-edit', args=[self.day.year, self.day.month, self.day.day]),
+    }
+
+    return self.render_to_response({})
