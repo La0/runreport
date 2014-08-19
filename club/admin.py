73e4c52c1bedb9cf5788d4bf848f56e0262d0038
Bastien Abadie <babadie@ivsweb.com>
2014-08-19 14:22:00 +0200

    Add admin view to send invites asked.

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,5 @@
+from coffin.conf.urls import patterns, url
+from django.shortcuts import get_object_or_404, render
 from django.contrib import admin
 from models import *
 
@@ -9,6 +11,27 @@ class ClubLinkAdmin(admin.TabularInline):
 
 class ClubInviteAdmin(admin.ModelAdmin):
   list_display = ('recipient', 'slug', 'club', 'type', 'sender', 'created')
+
+  def get_urls(self):
+    # Add send view to admin urls
+    urls = super(ClubInviteAdmin, self).get_urls()
+    my_urls = patterns('',
+      url(r'^(?P<invite_id>\d+)/send/?$', self.admin_site.admin_view(self.send_view), name="club-invite-send"),
+    )
+    return my_urls + urls
+
+  def send_view(self, request, invite_id):
+    '''
+    Ask for a confirmation before sending
+    '''
+    invite = get_object_or_404(ClubInvite, pk=invite_id)
+    if request.method == 'POST':
+      invite.send()
+    context = {
+      'invite' : invite,
+    }
+    return render(request, 'admin/club/send.html', context)
+
 admin.site.register(ClubInvite, ClubInviteAdmin)
 
 class ClubAdmin(admin.ModelAdmin):
