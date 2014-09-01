ffecebd5d242eb57141ce78ebc5ba59b2978c465
Bastien Abadie <bastien.abadie@gmail.com>
2014-09-01 23:14:39 +0200

    Quick fix to display race sessions.

--- users/views/races.py
+++ users/views/races.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 from django.db.models import Sum
-from sport.models import SportDay, RaceCategory
+from sport.models import SportSession, RaceCategory
 from datetime import date
 
 class RacesView(TemplateView):
@@ -12,22 +12,21 @@ class RacesView(TemplateView):
     return context
 
   def get_races(self):
+    # TODO: handle multiple race parts per day (triathlon)
+
     # List races
-    all_races = SportDay.objects.filter(week__user=self.request.user, type='race')
-    future_races = all_races.filter(date__gt=date.today()).order_by('date')
-    past_races = all_races.filter(date__lte=date.today())
+    all_races = SportSession.objects.filter(day__week__user=self.request.user, type='race')
+    future_races = all_races.filter(day__date__gt=date.today()).order_by('day__date')
+    past_races = all_races.filter(day__date__lte=date.today())
 
     # Extract the categories from past_races
     cat_ids= [r['race_category'] for r in past_races.values('race_category').distinct() if r['race_category']]
     categories = RaceCategory.objects.filter(pk__in=cat_ids).order_by('name')
 
-    # Sum sessions time & distance per race
-    past_races = past_races.annotate(time_total=Sum('sessions__time'), distance_total=Sum('sessions__distance'))
-
     # Categorize races
     races = {}
     for c in categories:
-      races[c.id] = past_races.filter(race_category=c).order_by('time_total')
+      races[c.id] = past_races.filter(race_category=c).order_by('time')
 
     return {
       'future_races' : future_races,
