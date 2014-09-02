88d92f8a814d5377ff0302d4a68030a4adf95e6e
Bastien Abadie <bastien.abadie@gmail.com>
2014-09-02 14:43:51 +0200

    Fix bad time & distance total for triathln case.

--- users/views/races.py
+++ users/views/races.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
-from django.db.models import Sum
-from sport.models import SportSession, RaceCategory
+from django.db.models import Sum, Count
+from sport.models import SportSession, RaceCategory, SportDay
 from datetime import date
 
 class RacesView(TemplateView):
@@ -12,21 +12,34 @@ class RacesView(TemplateView):
     return context
 
   def get_races(self):
-    # TODO: handle multiple race parts per day (triathlon)
 
-    # List races
-    all_races = SportSession.objects.filter(day__week__user=self.request.user, type='race')
-    future_races = all_races.filter(day__date__gt=date.today()).order_by('day__date')
-    past_races = all_races.filter(day__date__lte=date.today())
+    # List races days
+    race_days = SportDay.objects.filter(week__user=self.request.user, sessions__type='race')
+    future_races = race_days.filter(date__gt=date.today()).order_by('date')
+    past_races = race_days.filter(date__lte=date.today())
 
     # Extract the categories from past_races
-    cat_ids= [r['race_category'] for r in past_races.values('race_category').distinct() if r['race_category']]
-    categories = RaceCategory.objects.filter(pk__in=cat_ids).order_by('name')
+    cat_ids = [r['sessions__race_category'] for r in past_races.values('sessions__race_category').distinct() if r['sessions__race_category']]
+    categories = dict([(c.id, c) for c in RaceCategory.objects.filter(pk__in=cat_ids).order_by('name')])
+
+    # Sum sessions time & distance per race
+    past_races = past_races.annotate(time_total=Sum('sessions__time'), distance_total=Sum('sessions__distance'), nb_sessions=Count('sessions'))
+
+    '''
+    for c in categories:
+      races[c.id] = past_races.filter(sessions__race_category=c).order_by('time_total')
+    '''
 
     # Categorize races
+    # Smartwer way : the past_races query is evaluated here
     races = {}
-    for c in categories:
-      races[c.id] = past_races.filter(race_category=c).order_by('time')
+    for r in past_races:
+      for c in r.sessions.filter(type='race').values('race_category').distinct():
+        cat_id = c['race_category']
+        cat = categories[cat_id]
+        if cat_id not in races:
+          races[cat_id] = []
+        races[cat_id].append(r)
 
     return {
       'future_races' : future_races,
