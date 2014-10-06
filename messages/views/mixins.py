9a7bf9e535a4ef69fa6d8e63518152591ed78486
Bastien Abadie <bastien.abadie@gmail.com>
2014-10-06 09:34:16 +0200

    Reload session edit only for owner user.

--- messages/views/mixins.py
+++ messages/views/mixins.py
@@ -15,7 +15,7 @@ class MessageSessionReload(JsonResponseMixin):
   def reload(self, session=None):
     # Reload boxes & close modal
     self.json_options = [JSON_OPTION_CLOSE, JSON_OPTION_NO_HTML, ]
-    if session:
+    if session and session.day.week.user == self.request.user:
       date = session.day.date
       self.json_boxes = {
         'session-%s-%d' % (date, session.pk) : reverse('sport-session-edit', args=(date.year, date.month, date.day, session.pk,)),
