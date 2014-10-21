ed8ab4f9348dc51119a1c890b58222544f1ccc83
Bastien Abadie <bastien.abadie@gmail.com>
2014-10-21 14:59:30 +0200

    Add privacy setting for tracks

--- users/models.py
+++ users/models.py
@@ -105,6 +105,7 @@ class Athlete(AthleteBase):
   privacy_stats = models.CharField(max_length=50, choices=PRIVACY_LEVELS, default='club')
   privacy_calendar = models.CharField(max_length=50, choices=PRIVACY_LEVELS, default='private')
   privacy_comments = models.CharField(max_length=50, choices=PRIVACY_LEVELS, default='club')
+  privacy_tracks = models.CharField(max_length=50, choices=PRIVACY_LEVELS, default='club')
 
   def search_category(self):
     if not self.birthday:
