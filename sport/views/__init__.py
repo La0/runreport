64ff2cfef9afc2d1098e7c4d611ca155d663af4e
Bastien Abadie <bastien.abadie@gmail.com>
2014-12-10 20:27:33 +0100

    Translate missing sport templates.

--- sport/views/__init__.py
+++ sport/views/__init__.py
@@ -3,6 +3,5 @@ from month import RunCalendar, ExportMonth
 from day import RunCalendarDay, RunCalendarDayDelete
 from report import WeeklyReport, WeekPublish
 from year import RunCalendarYear
-from add import SessionAdd
 from session import SportSessionView, SportSessionDelete
 from stats import SportStats
