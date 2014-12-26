09b848cf164c03781505d4f0dff965da41c54a76
Bastien Abadie <bastien.abadie@gmail.com>
2014-12-26 09:42:25 +0100

    Add month/year selector while listing sessions.

--- post/forms.py
+++ post/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.forms.extras.widgets import SelectDateWidget
+from django.db.models import Min, Max
+from django.utils import timezone
 from post.models import Post
 
 class PostForm(forms.ModelForm):
@@ -6,3 +9,17 @@ class PostForm(forms.ModelForm):
     model = Post
     fields = ('title', 'slug', 'html', 'published', 'type', )
 
+class YearMonthForm(forms.Form):
+  date = forms.DateField(widget=SelectDateWidget())
+
+  def __init__(self, user, *args, **kwargs):
+    super(YearMonthForm, self).__init__(*args, **kwargs)
+
+    # Get min & max year for user's SportWeek
+    agg = user.sportweek.aggregate(min=Min('year'), max=Max('year'))
+    now = timezone.now()
+    min = agg.get('min', None) or now.year
+    max = agg.get('max', None) or now.year
+    years = range(min, max+1)
+    years.reverse()
+    self.fields['date'].widget.years = years
--- post/views/edit.py
+++ post/views/edit.py
@@ -3,6 +3,8 @@ from .mixins import PostWriterMixin
 from coach.mixins import JsonResponseMixin, JSON_OPTION_ONLY_AJAX
 from sport.models import SportSession
 from django.utils import timezone
+from post.forms import YearMonthForm
+from datetime import date
 
 class PostListView(PostWriterMixin, ListView):
   context_object_name = 'posts'
@@ -34,26 +36,38 @@ class PostSessionsView(PostWriterMixin, JsonResponseMixin, DetailView):
 
   def get_context_data(self, *args, **kwargs):
     context = super(PostSessionsView, self).get_context_data(*args, **kwargs)
+    context.update(self.load_date_form())
     context.update(self.load_user_sessions())
     return context
+
+
+  def load_date_form(self):
+    if 'switch' in self.request.POST:
+      # Use date from POST
+      self.date = date(year=int(self.request.POST['date_year']), month=int(self.request.POST['date_month']), day=int(self.request.POST['date_day']))
+    else:
+      # Defaults to today
+      self.date = date.today()
+    data = {
+      'date' : self.date,
+    }
+    form = YearMonthForm(self.request.user, data=data)
+    return {
+      'date' : self.date,
+      'date_form' : form,
+    }
 
   def load_user_sessions(self):
     '''
     List user sport sessions, one month at a time
     Default to current month
     '''
-    now = timezone.now()
-    month = self.request.POST.get('month', now.month)
-    year = self.request.POST.get('year', now.year)
-
     sessions = SportSession.objects.filter(day__week__user=self.request.user)
-    sessions = sessions.filter(day__date__month=month, day__date__year=year)
+    sessions = sessions.filter(day__date__month=self.date.month, day__date__year=self.date.year)
     sessions = sessions.prefetch_related('day', 'day__week')
     sessions = sessions.order_by('day__date')
 
     return {
-      'month' : month,
-      'year' : year,
       'user_sessions' : sessions,
       'post_sessions' : self.object.sessions.values_list('pk', flat=True),
     }
