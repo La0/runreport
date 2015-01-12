9e7f132054a5c83744ea45dbb0691b0c4f2283fd
Bastien Abadie <bastien.abadie@gmail.com>
2015-01-12 09:11:27 +0100

    Quickly change track's session.

--- tracks/views/__init__.py
+++ tracks/views/__init__.py
@@ -1,3 +1,3 @@
-from view import TrackCoordsView
+from view import TrackCoordsView, TrackSessionView
 from oauth import TrackOauthRedirect
 from providers import TrackProviders, TrackProviderDisconnect
