d44ab52c78f7224dfaed53246c49665cdcfdf327
Bastien Abadie <bastien.abadie@gmail.com>
2015-03-12 10:07:12 +0100

    Build unique username for subscriptions, refs #137.

--- club/tasks.py
+++ club/tasks.py
@@ -9,18 +9,31 @@ def subscribe_athlete(club, email, first_name, last_name):
   '''
   from users.models import Athlete
   from club.models import ClubMembership
+  from helpers import nameize
+
+  # Build a unique username
+  base = '%s_%s' % (nameize(first_name), nameize(last_name))
+  i = 1
+  username = None
+  while username is None:
+    username = i == 1 and base or '%s_%d' % (base, i)
+    try:
+      Athlete.objects.get(username=username)
+      username = None
+    except Athlete.DoesNotExist:
+      pass # use this username
+
+    i += 1
 
   # Check existing user
   defaults = {
+    'username' : username,
     'first_name' : first_name,
     'last_name' : last_name,
   }
   user, created = Athlete.objects.get_or_create(email=email, defaults=defaults)
   if not created:
     return
-
-  # Build a unique username
-  # TODO
 
   # Build an avatar for new user
   user.build_avatar()
