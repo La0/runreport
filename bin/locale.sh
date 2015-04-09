ba2ca6a083ed89d3f088f3b805d6591ad18a56cf
Bastien Abadie <bastien.abadie@gmail.com>
2015-04-09 19:45:52 +0200

    Update locales.

--- bin/locale.sh
+++ bin/locale.sh
@@ -1,1 +1,1 @@
-./manage.py makemessages -v 1 -l fr -l en -e html -i "*.txt" -i "*.json" -i "*.md"  -i "celery*" -i "*.sh" -i "medias" -i "final" -i "mails_debug"
+./manage.py makemessages -v 1 -l fr -l en -e html -i "*.txt" -i "*.json" -i "*.md"  -i "celery*" -i "*.sh" -i "medias" -i "final" -i "mails_debug" --keep-obsolete --no-location
