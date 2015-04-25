e5105a56dbff485107918ddbcdd52539f1328e42
Bastien Abadie <bastien.abadie@gmail.com>
2015-04-25 15:26:37 +0200

    Fix bug in session type translation display.

--- sport/models/__init__.py
+++ sport/models/__init__.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 from django.utils.translation import ugettext_lazy as _
 SESSION_TYPES = (
-  ('training', _('training')),
-  ('race', _('race')),
-  ('rest', _('rest')),
+  ('training', _('Training')),
+  ('race', _('Race')),
+  ('rest', _('Rest')),
 )
 
 from .organisation import SportWeek, SportDay, RaceCategory
