590e66b546a2583e23f0ecc0cab1cfd87b32fbd9
Bastien Abadie <bastien.abadie@gmail.com>
2015-09-09 15:42:01 +0200

    Fix payment club link with slug.

--- payments/urls.py
+++ payments/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
 
   # Pay an offer
   url(r'^(?P<slug>\w+)/?$', PaymentOfferPay.as_view(), name="payment-offer"),
-  url(r'^(?P<slug>\w+)/(?P<club_slug>[\w\_]+)/?$', PaymentOfferPay.as_view(), name="payment-offer-club"),
+  url(r'^(?P<slug>\w+)/(?P<club_slug>[\w\-\_]+)/?$', PaymentOfferPay.as_view(), name="payment-offer-club"),
 
   # Payment status
   url(r'^/?$', PaymentStatus.as_view(), name="payment-status"),
