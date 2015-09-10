68f19c24f33a4169381de43efe06181d582f9e7b
Bastien Abadie <bastien.abadie@gmail.com>
2015-09-10 10:17:29 +0200

    Check minimum legal age for RunReport on user creation.

--- users/forms.py
+++ users/forms.py
@@ -6,6 +6,50 @@ from helpers import nameize
 import gnupg
 from tracks.providers.garmin import GarminProvider, GarminAuthException
 from django.utils.translation import ugettext_lazy as _
+from datetime import date
+
+class DateSelectorWidget(forms.widgets.MultiWidget):
+    '''
+    Multiple select inputs to build a date
+    From https://docs.djangoproject.com/en/1.8/ref/forms/widgets/
+    '''
+    def __init__(self, attrs=None):
+        year = date.today().year
+        years = [(y, y) for y in range(1900, year + 1)]
+        years.reverse()
+        months = [(m, m) for m in range(1,13)]
+        days = [(d, d) for d in range(1, 32)]
+        _widgets = (
+            forms.widgets.Select(attrs=attrs, choices=days),
+            forms.widgets.Select(attrs=attrs, choices=months),
+            forms.widgets.Select(attrs=attrs, choices=years),
+        )
+        super(DateSelectorWidget, self).__init__(_widgets, attrs)
+
+    def decompress(self, value):
+        print value
+        if value:
+            return [value.day, value.month, value.year]
+        return [None, None, None]
+
+    def format_output(self, rendered_widgets):
+        return ''.join(rendered_widgets)
+
+    def value_from_datadict(self, data, files, name):
+        datelist = [
+            widget.value_from_datadict(data, files, name + '_%s' % i)
+            for i, widget in enumerate(self.widgets)]
+        try:
+            D = date(
+                day=int(datelist[0]),
+                month=int(datelist[1]),
+                year=int(datelist[2]),
+            )
+        except ValueError:
+            return ''
+        else:
+            return D
+
 
 class UserForm(forms.ModelForm):
   class Meta:
@@ -13,6 +57,7 @@ class UserForm(forms.ModelForm):
     fields = ('first_name', 'last_name', 'email', 'birthday', 'vma', 'frequency', 'frequency_rest', 'height', 'weight', 'comment', 'license', 'auto_send', 'nb_sessions', 'default_sport', 'avatar', 'privacy_avatar', 'privacy_records', 'privacy_races', 'privacy_stats', 'privacy_calendar', 'privacy_comments', 'privacy_tracks', 'language', 'daily_trainer_mail', 'display_help', 'phone', )
     widgets = {
       'nb_sessions' : forms.Select(choices=[(i,i) for i in range(0,21)]),
+      'birthday' : DateSelectorWidget(),
     }
 
   def clean_email(self):
@@ -25,7 +70,7 @@ class UserForm(forms.ModelForm):
         try:
           self.instance.unsubscribe_mailing(m)
           self.instance.subscribe_mailing(m, email=email)
-        except Exception, e:
+        except Exception:
           continue
 
     return email
@@ -49,11 +94,13 @@ class UserPasswordForm(forms.Form):
       raise ValidationError('Verification invalide.')
     return self.cleaned_data
 
+
 class SignUpForm(forms.Form):
   firstname = forms.CharField(label=_('Firstname'))
   lastname = forms.CharField(label=_('Lastname'))
   password = forms.CharField(min_length=4, widget=forms.PasswordInput(), label=_('Password'))
   password_check = forms.CharField(min_length=4, widget=forms.PasswordInput(), label=_('Repeat your password'))
+  birthday = forms.DateField(label=_('Birthday'), widget=DateSelectorWidget())
   email = forms.EmailField(label=_('Email'))
 
   def __init__(self, invite=None, *args, **kwargs):
@@ -78,6 +125,17 @@ class SignUpForm(forms.Form):
     if len(users) > 0:
       raise ValidationError(_('An account already uses this email.'))
     return self.cleaned_data['email']
+
+  def clean_birthday(self):
+    '''
+    Check user is at least 13 years old
+    '''
+    bday = self.cleaned_data['birthday']
+    diff = date.today() - bday
+    if diff.days < 13 * 365:
+      raise forms.ValidationError(_('You must be at least 13 years old to create an account.'))
+
+    return bday
 
   def clean(self):
     '''
