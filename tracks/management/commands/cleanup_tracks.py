a505185fdf15a0f22fc7110bdd2faf556b573817
Bastien Abadie <bastien.abadie@gmail.com>
2015-09-16 14:44:17 +0200

    Workaround to kill tracks doubles. Track's session_id is NOT unique.

--- tracks/management/commands/cleanup_tracks.py
+++ tracks/management/commands/cleanup_tracks.py
@@ -1,43 +1,18 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
 from tracks.models import Track
-import os
-import shutil
+from django.db.models import Count
+
 
 class Command(BaseCommand):
 
   def handle(self, *args, **kwargs):
 
-    # Check error dir
-    error_dir = settings.TRACK_DATA + '_errors'
+    # Cleanup doublons
+    doubles = Track.objects.values('session_id').annotate(nb=Count('session_id')).filter(nb__gt=1)
+    if not doubles:
+      raise CommandError('No doubles found !')
 
+    for d in doubles:
+      print d['session_id']
 
-    # Cleanup providers
-    for f in self.listdir(settings.TRACK_DATA):
-
-      # Get track id
-      name = os.path.basename(f)
-      if '_' not in name:
-        raise Exception('Invalid file %s' % f)
-      pk = int(name[:name.index('_')])
-
-      # Fetch track
-      try:
-        Track.objects.get(pk=pk)
-      except:
-        print 'err : %s' % f
-        # Delete file
-        os.unlink(f)
-
-  def listdir(self, path):
-    '''
-    Traverse recursively a dir, using generators
-    '''
-    for f in os.listdir(path):
-      full = os.path.join(path, f)
-      if os.path.isdir(full):
-        for sub in self.listdir(full):
-          yield sub
-      else:
-        yield full
-
+      Track.objects.filter(session_id=d['session_id']).last().delete()
