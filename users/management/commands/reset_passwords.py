e52cc70a7520bffd36fa0454c3875805da9ebb90
Bastien Abadie <bastien.abadie@gmail.com>
2015-10-30 10:59:37 +0100

    Ultra fast reset_passwords.

--- users/management/commands/reset_passwords.py
+++ users/management/commands/reset_passwords.py
@@ -1,17 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
 from users.models import Athlete
 from django.conf import settings
+from django.contrib.auth.hashers import make_password
 
 class Command(BaseCommand):
+
+  def add_arguments(self, parser):
+    parser.add_argument('password', type=str, nargs='?', default='plop')
 
   def handle(self, *args, **options):
     if not settings.DEBUG:
       raise CommandError('This command should not be run in production !')
-    self.reset(len(args) == 1 and args[0] or 'plop')
+    self.reset(options['password'])
 
   def reset(self, password):
-    for user in Athlete.objects.filter(demo=False):
-      print user
-      user.set_password(password)
-      user.save()
-    print 'Resetted those accounts with password %s' % password
+    password_hash = make_password(password)
+    athletes = Athlete.objects.all()
+    athletes.update(password=password_hash)
+    print 'Resetted %s accounts with password %s' % (athletes.count(), password)
