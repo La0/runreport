d3547486e0842d742706119de21fb3122f1520a5
Bastien Abadie <bastien@nextcairn.com>
2018-01-09 14:47:50 +0100

    upgrade : Disable csrf custom extension

--- src/runreport/settings.py
+++ src/runreport/settings.py
@@ -151,7 +151,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
-    'runreport.csrf.SubDomainCSRFMiddleware',
+    #'runreport.csrf.SubDomainCSRFMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
